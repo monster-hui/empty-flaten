7020adfc18e588aa0b18e42c2d9e31345d77811e
monster-hui <373000945@qq.com>
2016-05-18 10:43:45 +0800

    delete  error
    删除重复

--- practices/flaten/double_dimensional_array_to_one_2.js
+++ practices/flaten/double_dimensional_array_to_one_2.js
@@ -2,7 +2,6 @@
 
 function double_to_one(collection) {
 	collection=collection.join(',').split(',');
-	collection=collection.join(',').split(',');
 	for(var i=0;i<collection.length;i++){
 		collection[i]=parseInt(collection[i]);
 	}
