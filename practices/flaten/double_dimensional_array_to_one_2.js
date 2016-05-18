769561f163bcc588f61b6c18131f1e4399b9658e
monster-hui <373000945@qq.com>
2016-05-18 10:15:33 +0800

    add  function
    补充函数体

--- practices/flaten/double_dimensional_array_to_one_2.js
+++ practices/flaten/double_dimensional_array_to_one_2.js
@@ -1,8 +1,15 @@
 'use strict';
 
 function double_to_one(collection) {
-
-  //在这里写入代码
+	collection=collection.join(',').split(',');
+ 	var result = [], hash = {};
+	for (var i = 0, elem; (elem = collection[i]) != null; i++) {
+  	if (!hash[elem]) {
+  		result.push(elem);
+  		hash[elem] = true;
+  	}
+	}
+  return result ; 
 }
 
 module.exports = double_to_one;
