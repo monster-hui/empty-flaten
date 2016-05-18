3f6dfa0bf8b9944e697e5c910560e7d88f5224f2
monster-hui <373000945@qq.com>
2016-05-18 10:33:38 +0800

    update
    改进函数

--- practices/flaten/double_dimensional_array_to_one_1.js
+++ practices/flaten/double_dimensional_array_to_one_1.js
@@ -2,6 +2,9 @@
 
 function double_to_one(collection) {
   collection=collection.join(',').split(',');
+	for(var i=0;i<collection.length;i++){
+		collection[i]=parseInt(collection[i]);
+	}
   return collection ;
 }
 
